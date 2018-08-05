ea1e4311fc208960cc2f7e52fa41c52010faf561
DRAKE RENTROP <drakerentrop@gmail.com>
2018-08-05 23:16:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ var height = "74"
 if (name ==="SUsan") {
   console.log("The name is correct")
 }
-
-// Don't worry about this
-module.exports = { name, height
 } else {
   console.log("Expected the name to be Susan")
 }
+// Don't worry about this
+module.exports = { name, height
+
 
